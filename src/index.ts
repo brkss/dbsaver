9cf25b1fae7fe3460271edb69e1cad99ead9e901
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-05-24 00:39:18 +0100

    🍕 upload file to dropbox

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,34 @@ import "dotenv/config";
 import express from "express";
 import { schedule } from "node-cron";
 import { Dropbox } from "dropbox";
+import fs from "fs";
+import path from "path";
 
 const dbx = new Dropbox({ accessToken: process.env.ACCESS_TOKEN });
+
+const readFl = (p: string): Buffer => {
+  const res = fs.readFileSync(p);
+  return res;
+};
 
 (() => {
   const app = express();
 
+  const fileconfig = {
+    path: path.join(__dirname, "../test.txt"),
+  };
+
   dbx
-    .filesListFolder({ path: "" })
-    .then((response: any) => {
-      console.log(response);
+    .filesUpload({ path: "/test.txt", contents: readFl(fileconfig.path) })
+    .then((res) => {
+      console.log("res => ", res.result);
     })
-    .catch((err: any) => {
-      console.log("ERR => ", err);
+    .catch((e) => {
+      console.log("something went wrong trying to upload file : ", e);
     });
+  //dbx.filesUpload(arg)
+
+  readFl(path.join(__dirname, "../test.txt"));
 
   schedule("32 * * * *", () => {
     console.log("cron job running !");
