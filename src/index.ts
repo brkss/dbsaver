382f9c9d189facf9dd3d45f3a7b2b2a5507ceae1
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-05-23 22:32:27 +0100

    ⏰ setup cron job !

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { schedule } from "node-cron";
 
 (() => {
   const app = express();
+
+  schedule("32 * * * *", () => {
+    console.log("cron job running !");
+  });
 
   app.listen(4000, () => {
     console.log("🚀 DBSAVER RUNNING ON : http://localhost:4000");
