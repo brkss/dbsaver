c1612153d37dd30300abbdf1547962dd858846f1
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-05-24 01:10:56 +0100

    🦄 execute command to save sql file from mysql docker container w

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import "dotenv/config";
 import express from "express";
 import { schedule } from "node-cron";
-import { Dropbox } from "dropbox";
-import fs from "fs";
-import path from "path";
+//import { Dropbox } from "dropbox";
+//import fs from "fs";
+//import path from "path";
+import { createFileName } from "./file";
+import { saveDockerSqlFile } from "./execute";
 
-const dbx = new Dropbox({ accessToken: process.env.ACCESS_TOKEN });
-
+//const dbx = new Dropbox({ accessToken: process.env.ACCESS_TOKEN });
+/*
 const readFl = (p: string): Buffer => {
   const res = fs.readFileSync(p);
   return res;
+};
+*/
+const save = () => {
+  const filename = createFileName();
+  saveDockerSqlFile(filename);
+  console.log("✅ DONE !");
 };
 
 (() => {
   const app = express();
 
+  save();
+
+  /*
   const fileconfig = {
     path: path.join(__dirname, "../test.txt"),
   };
@@ -29,7 +40,7 @@ const readFl = (p: string): Buffer => {
     });
 
   readFl(path.join(__dirname, "../test.txt"));
-
+*/
   schedule("32 * * * *", () => {
     console.log("cron job running !");
   });
